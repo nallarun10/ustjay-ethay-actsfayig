110243061ada4203d682de1aeef8b8152faf24dd
Arun Nalla <nalla.arunkumar@gmail.com>
2019-05-09 22:54:06 -0700

    Class05 Assignment

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import os
+#! usr/bin/ env python3
 
+import os
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 def get_fact():
+    '''Extract random facts from the url using request libary ans get method'''
+    '''parse using Beautifulsuop and pull out content'''
 
     response = requests.get("http://unkno.com")
 
@@ -16,13 +18,23 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_quote(fact):
+    """ the above extracted content need to be entered in box '<form> POST input_text"""
+
+    data = {"input_text": fact}
+
+    r = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", allow_redirects=False, data=data)
+    ''' extract the newly formed url'''
+    return r.headers['Location']
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    fact = get_fact().strip()
+    mashup_link = get_quote(fact)
+    return mashup_link
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
+
 
